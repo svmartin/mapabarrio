ded76442912f59d2f4dcf8ad2d780f9afcef4e15
SM <sean.martin@gmail.com>
2017-10-09 17:58:19 -0500

    add snappymaps theme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Barrio Map
+http://manager-eileen-55622.netlify.com
--- js/app.js
+++ js/app.js
@@ -4,8 +4,11 @@ var markers = [];
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 44.977753, lng: -93.265011},
-    zoom: 13
+    center: {lat: 44.9689133, lng: -93.21455600000002},
+    zoom: 13,
+    // style from SnazzyMaps: https://snazzymaps.com/style/25/blue-water
+    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+
   });
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
